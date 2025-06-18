645fb393592bd83c4ae896bfaadd7a1cfe650f02
Luiz Passos <lsps1019@gmail.com>
2025-06-17 23:20:04 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -25,14 +25,55 @@ WHERE ANAMINESE.data_treino >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH);
 SELECT ALUNO.nome, ALUNO.matricula, ALUNO.CREF 
 FROM ALUNO
 JOIN
-	(SELECT data, MENSALIDADE_PLANO.matricula, nome_plano, data_pagamento FROM MENSALIDADE_PLANO
+	(
+	SELECT data, MENSALIDADE_PLANO.matricula, nome_plano, data_pagamento 
+	FROM MENSALIDADE_PLANO
 	JOIN 
-		(SELECT matricula, MAX(data) AS data_pagamento FROM MENSALIDADE WHERE status = TRUE GROUP BY matricula) AS ultimo_pagamento
+		(
+		SELECT matricula, MAX(data) AS data_pagamento FROM MENSALIDADE WHERE status = TRUE GROUP BY matricula
+		) AS ultimo_pagamento
 	ON MENSALIDADE_PLANO.matricula = ultimo_pagamento.matricula AND MENSALIDADE_PLANO.data = ultimo_pagamento.data_pagamento)
 	AS matricula_mensalidade ON ALUNO.matricula = matricula_mensalidade.matricula
 WHERE
-	(matricula_mensalidade.nome_plano = 'Mensal' AND matricula_mensalidade.data_pagamento <= DATE_SUB(CURDATE(), INTERVAL 1 MONTH))
+	(
+	matricula_mensalidade.nome_plano = 'Mensal' AND matricula_mensalidade.data_pagamento <= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+	)
 	OR 
-	(matricula_mensalidade.nome_plano = 'Trimestral' AND matricula_mensalidade.data_pagamento <= DATE_SUB(CURDATE(), INTERVAL 3 MONTH))
+	(
+	matricula_mensalidade.nome_plano = 'Trimestral' AND matricula_mensalidade.data_pagamento <= DATE_SUB(CURDATE(), INTERVAL 3 MONTH)
+	)
 	OR 
-	(matricula_mensalidade.nome_plano = 'Anual' AND matricula_mensalidade.data_pagamento <= DATE_SUB(CURDATE(), INTERVAL 12 MONTH));
+	(
+	matricula_mensalidade.nome_plano = 'Anual' AND matricula_mensalidade.data_pagamento <= DATE_SUB(CURDATE(), INTERVAL 12 MONTH)
+	);
+
+-- 5. Total pago em mensalidades por mês no ano corrente
+SELECT MONTH(data) AS mes, SUM(valor) AS total_mensalidades
+FROM
+	(
+	SELECT nome_mensalidade.data, nome_mensalidade.matricula, nome_mensalidade.status, PLANO.nome, PLANO.valor
+	FROM
+		(
+		SELECT MENSALIDADE.data, MENSALIDADE.matricula, MENSALIDADE.status, MENSALIDADE_PLANO.nome_plano 
+		FROM MENSALIDADE 
+		JOIN MENSALIDADE_PLANO ON MENSALIDADE.matricula = MENSALIDADE_PLANO.matricula AND MENSALIDADE.data = MENSALIDADE_PLANO.data
+		) AS nome_mensalidade
+	JOIN
+		PLANO ON nome_mensalidade.nome_plano = PLANO.nome
+	) AS valor_mensalidade
+WHERE YEAR(data) = YEAR(CURDATE())
+GROUP BY MONTH(data);
+
+-- 6. Quantidade de alunos por professor no último ano (dúvida: o que acontece com alunos que pagaram mensalidade em 2023 
+-- e tem matrícula ativa em 2024?). Por exemplo: alguém que pagou o plano anual em dezembro de 2023
+-- Seria interessante ter o status da matricula, registrando se a matricula do aluno está ativa ou não?
+SELECT aluno_professor.nome_professor, COUNT(DISTINCT aluno_professor.matricula) AS numero_alunos
+FROM
+	(
+	SELECT a.matricula, a.nome AS nome_aluno, pr.nome AS nome_professor  
+	FROM ALUNO a
+	JOIN PROFESSOR_RESPONSAVEL pr ON a.CREF = pr.CREF
+	) AS aluno_professor
+JOIN MENSALIDADE_PLANO mp ON aluno_professor.matricula = mp.matricula
+WHERE YEAR(DATE_SUB(CURDATE(), INTERVAL 12 MONTH)) = YEAR(mp.data)
+GROUP BY aluno_professor.nome_professor;
