44140df259a62586ca049da1073d7a62707e1b90
Luiz Passos <lsps1019@gmail.com>
2025-06-25 23:22:05 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,25 +1,39 @@
 -- 1. Média de treinos por dia da semana
-SELECT treinos.dia_semana, AVG(treinos.treinos_dia) AS media, COUNT(treinos.dia_semana) AS qtd_dias
-FROM ( SELECT data_treino, DAYNAME(data_treino) AS dia_semana, COUNT(*) AS treinos_dia  FROM ALUNO_TREINO GROUP BY data_treino) AS treinos 
-GROUP BY treinos.dia_semana ORDER BY FIELD(treinos.dia_semana, 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+SELECT 
+	treinos.dia_semana, 
+	AVG(treinos.treinos_dia) AS media,
+	COUNT(treinos.dia_semana) AS qtd_dias
+FROM ( 
+	SELECT 
+		data_treino,
+		DAYNAME(data_treino) AS dia_semana,
+		COUNT(*) AS treinos_dia
+	FROM ALUNO_TREINO 
+	GROUP BY data_treino) AS treinos 
+GROUP BY treinos.dia_semana 
+ORDER BY FIELD(treinos.dia_semana, 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
 
 -- 2. Ranqueamento dos treinos mais utilizados pelos alunos
-SELECT tipo AS tipo_treino, COUNT(tipo) AS numero_treinos FROM ALUNO_TREINO GROUP BY tipo ORDER BY numero_treinos DESC;
+SELECT 
+	tipo AS tipo_treino, 
+	COUNT(tipo) AS numero_treinos 
+FROM ALUNO_TREINO 
+GROUP BY tipo 
+ORDER BY numero_treinos DESC;
 
 -- 3. Quantidade de alunos em cada faixa de IMC no último mês
 SELECT
-    ALUNO.matricula, 
-    ALUNO.nome, 
-    (ANAMINESE.peso / (ANAMINESE.altura * ANAMINESE.altura)) AS imc, 
-    CASE
+	CASE
         WHEN (ANAMINESE.peso / (ANAMINESE.altura * ANAMINESE.altura)) < 18.5 THEN 'Abaixo do peso' 
         WHEN (ANAMINESE.peso / (ANAMINESE.altura * ANAMINESE.altura)) BETWEEN 18.5 AND 24.9 THEN 'Peso normal' 
         WHEN (ANAMINESE.peso / (ANAMINESE.altura * ANAMINESE.altura)) BETWEEN 25 AND 29.9 THEN 'Sobrepeso' 
         ELSE 'Obesidade'
-    END AS faixa_imc
+    END AS faixa_imc,
+    COUNT(ALUNO.matricula) AS qtd_alunos  
 FROM ALUNO 
 INNER JOIN ANAMINESE  ON ALUNO.matricula = ANAMINESE.matricula 
-WHERE ANAMINESE.data_treino >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH);
+WHERE ANAMINESE.data_treino >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+GROUP BY faixa_imc;
 
 -- 4. Alunos inadimplentes
 SELECT ALUNO.nome, ALUNO.matricula, ALUNO.CREF 
@@ -37,15 +51,11 @@ JOIN
 WHERE
 	(
 	matricula_mensalidade.nome_plano = 'Mensal' AND matricula_mensalidade.data_pagamento <= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
-	)
-	OR 
-	(
+	) OR (
 	matricula_mensalidade.nome_plano = 'Trimestral' AND matricula_mensalidade.data_pagamento <= DATE_SUB(CURDATE(), INTERVAL 3 MONTH)
-	)
-	OR 
-	(
+	) OR (
 	matricula_mensalidade.nome_plano = 'Anual' AND matricula_mensalidade.data_pagamento <= DATE_SUB(CURDATE(), INTERVAL 12 MONTH)
-	);
+);
 
 -- 5. Total pago em mensalidades por mês no ano corrente
 SELECT MONTH(data) AS mes, SUM(valor) AS total_mensalidades
